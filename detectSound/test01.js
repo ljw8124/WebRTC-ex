61be285d4d8ba67b840027a8f50a6958ae97fe57
Joungwoo Lee <ljw8124@gmail.com>
2023-01-06 16:08:59 +0900

    Update test01.js

--- detectSound/test01.js
+++ detectSound/test01.js
@@ -37,9 +37,20 @@ function handleSuccess(stream)
             return;
         }
         
+        /*
+        interval를 사용하는 것보다 별개로 작동하는 requestAnimationFrame을 사용하는 것도 대안으로 가능.
+        ex)
+        const soundReqAni = requestAnimationFrame(반복할 함수명);
+        
+        clear 할때는 cancelAnimationFrame(함수명);
+        */
+        
+        // 이 때 clear 는 clearInterval()
         meterRefresh = setInterval(() => {
             //soundmeter에서는 for를 너무 많이 돌아서 여기서 감지하는 것이 좋음
             console.warn(soundMeter.detectedVolume.toFixed(2));
+            // detectedVolume 을 이용해서 값에 따라 말하고 있는지 체크가 가능하고,
+            // <meter> 를 이용하여 음성 인식이 가능하게끔 개발이 가능함
         }, 200);
         
     });
@@ -59,7 +70,7 @@ function stop() {
     window.soundMeter.stop();
     window.audioContext.close();
     clearInterval(meterRefresh);
-   
+   // 만약 meterRefresh 가 죽지 않는다면, start 할 때 meterRefresh 가 존재하는지 점검하고 clear 하는게 
 }
 
 document.getElementById('start').onclick = start;
